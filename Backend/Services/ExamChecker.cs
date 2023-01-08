c5bab547302112a6e8ac6f3dfd27fb1d80d545f2
Fedor Surov <fyodor.s2013@yandex.ru>
2023-01-08 16:20:24 +0300

    examChecker created

---
+++ Backend/Services/ExamChecker.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Models;
+
+namespace Backend.Services
+{
+    public class ExamChecker
+    {
+        public ExamCheckerResponse Check(Exam exam)
+        {
+            int currentGrade = 0;
+            foreach (Question question in exam.Questions)
+            {
+                currentGrade += question.Answers.FirstOrDefault(x => x.IsChosen == true).Cost;
+            }
+            currentGrade *= 100;
+            return new ExamCheckerResponse() { percent = currentGrade / exam.PassingGrade+1 ,passed = currentGrade>exam.PassingGrade };
+        }
+    }
+}
