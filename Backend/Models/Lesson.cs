8691cd59832bb92e83049906a6c97adb43fe5e82
ProdamGaraj <prodgaraj@yandex.ru>
2023-01-12 20:39:36 +0300

    Fixed Lesson type prop.

--- Backend/Models/Lesson.cs
+++ Backend/Models/Lesson.cs
@@ -4,7 +4,7 @@ namespace Backend.Models
 {
     public class Lesson:Entity
     {
-        public LessonType LesssonType { get; set; }
+        public LessonType LessonType { get; set; }
         public string Description { get; set; }
         public string TextContent { get; set; }
         public string MediaContentPath { get; set; }
@@ -12,7 +12,7 @@ namespace Backend.Models
         {}
         public Lesson(LessonType lesssonType, string description, string textContent, string mediaContentPath, bool deleted=false)
         {
-            LesssonType = lesssonType;
+            LessonType = lesssonType;
             Description = description;
             TextContent = textContent;
             MediaContentPath = mediaContentPath;
