ba29bb18708da9ccee8447502ebae7c580ef126b
Fedor Surov <fyodor.s2013@yandex.ru>
2023-01-13 02:23:48 +0300

    Controller abd default page done

---
+++ Backend/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using Backend.Models;
+using Backend.Services;
+using Backend.Services.Repository;
+using Backend.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Buy() => View();
+        [HttpPost]
+        public async Task<IActionResult> Buy(BuyViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                CourseService cs = new CourseService();
+                vm.Course = (await cs.GetCourse(vm)).Data;
+            }
+            return View(vm);
+        }
+    }
+}
