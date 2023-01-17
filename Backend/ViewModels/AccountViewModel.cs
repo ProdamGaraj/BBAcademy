8546692c874ea8b335401aaf4ed9927da4d18efe
ProdamGaraj <prodgaraj@yandex.ru>
2023-01-17 20:40:12 +0300

    Added: AccountViewModel.cs Modified Account Index view.

---
+++ Backend/ViewModels/AccountViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Models;
+namespace Backend.ViewModels
+{
+    public class AccountViewModel
+    {
+        public User User{ get; set; }
+        public List<Course> AllCourses { get; set; }
+        public List<Course> BoughtCourses { get; set; }
+        public List<Course> EndedCourses { get; set; }
+    }
+}
