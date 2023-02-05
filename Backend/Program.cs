7df393795450739d1971162a94af053dd1ef4ab1
Fedor Surov <fyodor.s2013@yandex.ru>
2023-02-06 02:31:34 +0300

    Data addition added

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -70,6 +70,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseStaticFiles(new StaticFileOptions
 {
 	FileProvider = new PhysicalFileProvider(builder.Environment.ContentRootPath)
