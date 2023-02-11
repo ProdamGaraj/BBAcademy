2574f735495e9121ce3bc04cd0f115202c40122d
ProdamGaraj <prodgaraj@yandex.ru>
2023-02-11 20:00:16 +0300

    landing styles general update

--- bb-frontend/src/components/Landing/landing.js
+++ bb-frontend/src/components/Landing/landing.js
@@ -1,168 +1,171 @@
 ﻿import translations from 'translations'
 import LangContext from "../../contexts/lang-context";
 import {useContext} from "react";
-import {Loader} from "../loader/loader";
 
-export default ({match}) => {
+import styles from './landing.module.css'
 
-    let currentLang = useContext(LangContext).lang
+export default () => {
+
+    let lang = useContext(LangContext).lang
 
     return (<>
-        <div className="block-learn-in">
-            <img className="block-learn-in_left-section" src="/img/Home/guy3.png"/>
-            <div className="block-learn-in_right-section">
-                <div className="block-learn-in_right-section-top">
-                    <div
-                        className="block-learn-in_right-section-top-top">{translations[currentLang].learnWithBBAcademy}</div>
-                    <div
-                        className="block-learn-in_right-section-top-bot">{translations[currentLang].improveYourProfessionalCapital}</div>
-                </div>
-                <div className="block-learn-in_right-section-bot"></div>
-            </div>
-        </div>
-        <div className="adv-learn">
-            <div className="adv-top">
-                {translations[currentLang].investYourself}
-            </div>
-            <div className="adv-learn-right">
-                <div className="adv-learn-right-block">
-                    <div className="adv-card-book">
-                        <img className="book-svg" src="/img/Home/book.svg"/>
+        <main role="main" className={styles.pb3}>
+            <div className={styles.blockLearnIn}>
+                <img className={styles.blockLearnInLeftSection} src="/img/Home/guy3.png" alt=""/>
+                <div className={styles.blockLearnInRightSection}>
+                    <div className={styles.blockLearnInRightSectionTopTop}>
+                        {translations[lang].learnWithBBAcademy}
                     </div>
-                    <div className="card-text">{translations[currentLang].aLotOfCourses}</div>
-                </div>
-                <div className="adv-learn-right-block">
-                    <div className="adv-card-book">
-                        <img className="book-svg" src="/img/Home/book.svg"/>
-                    </div>
-                    <div className="card-text">{translations[currentLang].rateSystem}</div>
-                </div>
-                <div className="adv-learn-right-block">
-                    <div className="adv-card-book">
-                        <img className="book-svg" src="/img/Home/book.svg"/>
-                    </div>
-                    <div className="card-text">{translations[currentLang].autoExamination}</div>
-                </div>
-                <div className="adv-learn-right-block">
-                    <div className="adv-card-book">
-                        <img className="book-svg" src="/img/Home/book.svg"/>
-                    </div>
-                    <div className="card-text">{translations[currentLang].professorCommunication}</div>
-                </div>
-            </div>
-        </div>
-        <div className="video-block"></div>
-        <div className="category_and_popular">
-            <div className="category">
-                <div className="category-top">
-                    <div className="category-top-left">{translations[currentLang].categories}</div>
-                    <div className="category-top-right">{translations[currentLang].showMore}</div>
-                </div>
-                <div className="category-bot">
-                    <div className="category-bot-block">
-                        <div className="category-bot-block-text">
-                            <div className="category-bot-block-text-1">
-                                {translations[currentLang].bBanking}
-                            </div>
-                            <div className="category-bot-block-text-2">
-                                {translations[currentLang].courses14}
-                            </div>
-                        </div>
-                        <img className="category-percent" src="/img/Home/percent.svg"/>
-                        <div className="cat-circle-1"></div>
-                    </div>
-                    <div className="category-bot-block">
-                        <div className="category-bot-block-text">
-                            <div className="category-bot-block-text-1">
-                                {translations[currentLang].bBanking}
-                            </div>
-                            <div className="category-bot-block-text-2">
-                                {translations[currentLang].courses14}
-                            </div>
-                        </div>
-                        <img className="category-percent" src="/img/Home/percent.svg"/>
-                        <div className="cat-circle-2"></div>
-                    </div>
-                    <div className="category-bot-block">
-                        <div className="category-bot-block-text">
-                            <div className="category-bot-block-text-1">
-                                {translations[currentLang].bBanking}
-                            </div>
-                            <div className="category-bot-block-text-2">
-                                {translations[currentLang].courses14}
-                            </div>
-                        </div>
-                        <img className="category-percent" src="/img/Home/percent.svg"/>
-                        <div className="cat-circle-3"></div>
-                    </div>
-                    <div className="category-bot-block">
-                        <div className="category-bot-block-text">
-                            <div className="category-bot-block-text-1">
-                                {translations[currentLang].bBanking}
-                            </div>
-                            <div className="category-bot-block-text-2">
-                                {translations[currentLang].courses14}
-                            </div>
-                        </div>
-                        <img className="category-percent" src="/img/Home/percent.svg"/>
-                        <div className="cat-circle-4"></div>
+                    <div className={styles.blockLearnInRightSectionTopBot}>
+                        {translations[lang].improveYourProfessionalCapital}
                     </div>
                 </div>
             </div>
-            <div className="popular">
-                <div className="popular-top">
-                    <div className="popular-top-left">{translations[currentLang].popularCourses}</div>
-                </div>
-                <div className="popular-bot">
-                    <div className="popular-bot-block">
-                        <img className="popular-bot-block" src="/img/Shared/photo.png"/>
-                        <div className="popular-bot-block-deco-1">
-                            <div
-                                className="popular-bot-block-deco-title">{translations[currentLang].infestionForBeginners}</div>
-                        </div>
+            <div className={styles.advContainer}>
+                <div className={styles.advBlock}>
+                    <div className={styles.advTop}>
+                        {translations[lang].investYourself}
                     </div>
-                    <div className="popular-bot-block">
-                        <img className="popular-bot-block" src="/img/Shared/photo.png"/>
-                        <div className="popular-bot-block-deco-2">
-                            <div
-                                className="popular-bot-block-deco-title">{translations[currentLang].infestionForBeginners}</div>
+                    <div className={styles.advLearnRight}>
+                        <div className={styles.advLearnRightBlock}>
+                            <div className={styles.advCardBook}>
+                                <img className={styles.bookSvg} src="/img/Home/book.svg" alt=""/>
+                            </div>
+                            <div className={styles.cardText}>{translations[lang].aLotOfCourses}</div>
                         </div>
-                    </div>
-                    <div className="popular-bot-block">
-                        <img className="popular-bot-block" src="/img/Shared/photo.png"/>
-                        <div className="popular-bot-block-deco-3">
-                            <div
-                                className="popular-bot-block-deco-title">{translations[currentLang].infestionForBeginners}</div>
+                        <div className={styles.advLearnRightBlock}>
+                            <div className={styles.advCardBook}>
+                                <img className={styles.bookSvg} src="/img/Home/book.svg" alt=""/>
+                            </div>
+                            <div className={styles.cardText}>{translations[lang].rateSystem}</div>
+                        </div>
+                        <div className={styles.advLearnRightBlock}>
+                            <div className={styles.advCardBook}>
+                                <img className={styles.bookSvg} src="/img/Home/book.svg" alt=""/>
+                            </div>
+                            <div className={styles.cardText}>{translations[lang].autoExamination}</div>
+                        </div>
+                        <div className={styles.advLearnRightBlock}>
+                            <div className={styles.advCardBook}>
+                                <img className={styles.bookSvg} src="/img/Home/book.svg" alt=""/>
+                            </div>
+                            <div className={styles.cardText}>{translations[lang].professorCommunication}</div>
                         </div>
                     </div>
                 </div>
             </div>
-            <button className="show_more">
-                {translations[currentLang].showMore}
-            </button>
-        </div>
-        <div className="mobility">
-            <div className="mobility-left">
-                <div className="mobility-left-top">
-                    <div className="mobility-left-top-1">{translations[currentLang].moreStability}</div>
-                    <div className="mobility-left-top-2">{translations[currentLang].mobileApp}</div>
+            <div className="video-block"></div>
+            <div className={styles.categoryAndPopular}>
+                <div className={styles.category}>
+                    <div className={styles.categoryTop}>
+                        <div className={styles.categoryTopLeft}>{translations[lang].categories}</div>
+                        <div className={styles.categoryTopRight}>{translations[lang].showMore}</div>
+                    </div>
+                    <div className={styles.categoryBot}>
+                        <div className={styles.categoryBotBlock}>
+                            <div className={styles.categoryBotBlockText}>
+                                <div className={styles.categoryBotBlockTitle}>
+                                    {translations[lang].bBanking}
+                                </div>
+                                <div className={styles.categoryBotBlockDuration}>
+                                    {translations[lang].fourteenCourses}
+                                </div>
+                            </div>
+                            <img className={styles.categoryPercent} src="/img/Home/percent.svg" alt=""/>
+                            <div className={styles.catCircleSky}></div>
+                        </div>
+                        <div className={styles.categoryBotBlock}>
+                            <div className={styles.categoryBotBlockText}>
+                                <div className={styles.categoryBotBlockTitle}>
+                                    {translations[lang].bBanking}
+                                </div>
+                                <div className={styles.categoryBotBlockDuration}>
+                                    {/* BUG: UNKNOWN STRING */}{translations[lang].courses14}
+                                </div>
+                            </div>
+                            <img className={styles.categoryPercent} src="/img/Home/percent.svg" alt=""/>
+                            <div className={styles.catCircleBlue}></div>
+                        </div>
+                        <div className={styles.categoryBotBlock}>
+                            <div className={styles.categoryBotBlockText}>
+                                <div className={styles.categoryBotBlockTitle}>
+                                    {translations[lang].bBanking}
+                                </div>
+                                <div className={styles.categoryBotBlockDuration}>
+                                    {/* BUG: UNKNOWN STRING */}{translations[lang].courses14}
+                                </div>
+                            </div>
+                            <img className={styles.categoryPercent} src="/img/Home/percent.svg" alt=""/>
+                            <div className={styles.catCircleMint}></div>
+                        </div>
+                        <div className={styles.categoryBotBlock}>
+                            <div className={styles.categoryBotBlockText}>
+                                <div className={styles.categoryBotBlockTitle}>
+                                    {translations[lang].bBanking}
+                                </div>
+                                <div className={styles.categoryBotBlockDuration}>
+                                    {/* BUG: UNKNOWN STRING */}{translations[lang].courses14}
+                                </div>
+                            </div>
+                            <img className={styles.categoryPercent} src="/img/Home/percent.svg" alt=""/>
+                            <div className={styles.catCircleRed}></div>
+                        </div>
+                    </div>
                 </div>
-                <button className="mobility-left-download">{translations[currentLang].download}</button>
+                <div className={styles.popular}>
+                    <div className={styles.popularTop}>
+                        <div className={styles.popularTopLeft}>{translations[lang].popularCourses}</div>
+                    </div>
+                    <div className={styles.popularBot}>
+                        <div className={styles.popularBotBlock}>
+                            <img className={styles.popularBotBlock} src="/img/Shared/photo.png" alt=""/>
+                            <div className={styles.popularBotBlockBlue}>
+                                <div
+                                    className={styles.popularBotBlockTitle}>{translations[lang].infestionForBeginners}</div>
+                            </div>
+                        </div>
+                        <div className={styles.popularBotBlock}>
+                            <img className={styles.popularBotBlock} src="/img/Shared/photo.png" alt=""/>
+                            <div className={styles.popularBotBlockMint}>
+                                <div
+                                    className={styles.popularBotBlockTitle}>{translations[lang].infestionForBeginners}</div>
+                            </div>
+                        </div>
+                        <div className={styles.popularBotBlock}>
+                            <img className={styles.popularBotBlock} src="/img/Shared/photo.png" alt=""/>
+                            <div className={styles.popularBotBlockSky}>
+                                <div
+                                    className={styles.popularBotBlockTitle}>{translations[lang].infestionForBeginners}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <button className={styles.showMore}>
+                    {translations[lang].showMore}
+                </button>
             </div>
-            <div className="mobility-right">
-                <img src="/img/Home/Mask group.png"/>
+            <div className={styles.mobility}>
+                <div className={styles.mobilityLeft}>
+                        <div className={styles.mobilityTitle}>{translations[lang].moreStability}</div>
+                        <div className={styles.mobilityText}>{translations[lang].mobileApp}</div>
+                    <button className={styles.mobilityLeftDownload}>{translations[lang].download}</button>
+                </div>
+                <div className={styles.mobilityRight}>
+                    <img src="/img/Home/Mask group.png" alt=""/>
+                </div>
             </div>
-        </div>
-        <div className="partners">
-            <div className="partners-top">{translations[currentLang].ourPartners}</div>
-            <div className="partners-photo-block">
-                <img className="partners-photo" src="/img/Home/partn.png"/>
-                <img className="partners-photo" src="/img/Home/partn.png"/>
-                <img className="partners-photo" src="/img/Home/partn.png"/>
-                <img className="partners-photo" src="/img/Home/partn.png"/>
-                <img className="partners-photo" src="/img/Home/partn.png"/>
-                <img className="partners-photo" src="/img/Home/partn.png"/>
+            <div className={styles.partners}>
+                <div className={styles.partnersTop}>{translations[lang].ourPartners}</div>
+                <div className={styles.partnersPhotoBlock}>
+                    <img className="partners-photo" src="/img/Home/partn.png" alt=""/>
+                    <img className="partners-photo" src="/img/Home/partn.png" alt=""/>
+                    <img className="partners-photo" src="/img/Home/partn.png" alt=""/>
+                    <img className="partners-photo" src="/img/Home/partn.png" alt=""/>
+                    <img className="partners-photo" src="/img/Home/partn.png" alt=""/>
+                    <img className="partners-photo" src="/img/Home/partn.png" alt=""/>
+                </div>
             </div>
-        </div>
+
+        </main>
     </>)
 }
