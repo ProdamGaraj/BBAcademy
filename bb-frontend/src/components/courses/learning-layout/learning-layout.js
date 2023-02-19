75698942e32ce3b763d4aa5a829fdb767fabcffd
allemeshov <allemeshov@gmail.com>
2023-02-19 16:19:58 +0100

    feat(learning-page): responsive learning papge.

--- bb-frontend/src/components/courses/learning-layout/learning-layout.js
+++ bb-frontend/src/components/courses/learning-layout/learning-layout.js
@@ -1,17 +1,50 @@
-﻿import {NavLink} from "react-router-dom";
-import translations from "translations";
+﻿import translations from "translations";
 
 import styles from './learning-layout.module.css'
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import LangContext from "contexts/lang-context";
 
 const LESSON_MODE = 1;
 const EXAM_MODE = 2;
 const CERT_MODE = 3;
 
-export default ({children, course, toLesson, toExam, toCert, activeLessonIndex, activeMode}) => {
+export default (props) => {
 
-    let currentLang = useContext(LangContext).lang
+    const {children, course, toLesson, toExam, toCert, activeLessonIndex, activeMode} = props;
+
+    let currentLang = useContext(LangContext).lang;
+
+    const handleChapterChange = (value) => {
+        const selectedOptionId = value.target.selectedOptions[0].id;
+        if (selectedOptionId === 'exam') {
+            return toExam();
+        }
+        if (selectedOptionId === 'certificate') {
+            return toCert();
+        }
+
+        const selectedLessonIndex = value.target.selectedIndex;
+        return toLesson(selectedLessonIndex);
+    }
+
+    useEffect(() => {
+        document.getElementById('lesson-select').selectedIndex = activeLessonIndex;
+    }, [activeLessonIndex])
+
+    const getLastOption = (certName) => {
+        return certName === null ?
+            (
+                <option id={'exam'} key={'exam'}>
+                    {course.exam.title}
+                </option>
+            )
+            :
+            (
+                <option id={'certificate'} key={'certificate'}>
+                    {translations[currentLang].ending}
+                </option>
+            );
+    }
 
     return (<>
         <div className={styles.layout}>
@@ -49,6 +82,26 @@ export default ({children, course, toLesson, toExam, toCert, activeLessonIndex, 
                 <div className={styles.divider}/>
 
                 <div className={styles.content}>
+                    {children}
+                </div>
+            </div>
+
+            <div className={styles.layoutContainerMobile}>
+                <header className={styles.courseHeader}>
+                    <h1>{course.title}</h1>
+
+                    <nav className={styles.courseNavigation}>
+                        <label htmlFor="lesson-select">Lesson:</label>
+                        <select onChange={handleChapterChange} name="lesson" id="lesson-select">
+                            {course.lessons.map((lesson, i) =>
+                                <option key={i} value={lesson.title}>{lesson.title}</option>
+                            )}
+
+                            {getLastOption(course.certName)}
+                        </select>
+                    </nav>
+                </header>
+                <div className={styles.lessonContent}>
                     {children}
                 </div>
             </div>
