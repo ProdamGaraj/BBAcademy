ea63827c687bb9b9d56d1f6a1153139fd656be8a
Fedor Surov <fyodor.s2013@yandex.ru>
2023-02-25 15:59:36 +0300

    paymentDetails

---
+++ bb-backend/BLL/PaymentService/PaymentService.cs
@@ -0,0 +1,24 @@
+﻿using BLL.Models.Configs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.PaymentService
+{
+    internal class PaymentService : IPaymentService
+    {
+        public async Task<string> GetUrlForPurchase(long payment)
+        {
+            PaymentConfig paymentConfig = JsonConvert.DeserializeObject<PaymentConfig>(File.ReadAllText("PaymentDetails.json"));
+            string paymentUrl = $"https://my.click.uz/services/pay?service_id={paymentConfig.ServiceId}&merchant_id={paymentConfig.MerchantId}&amount={payment}&return_url={paymentConfig.Return_url}";
+            if (paymentConfig.CardType is not null)
+            {
+                paymentUrl += "&card_type =" + paymentConfig.CardType;
+            }
+            return paymentUrl;
+        }
+    }
+}
