f123fe273490777a797c9676b36eeaa7c168cf04
ProdamGaraj <prodgaraj@yandex.ru>
2023-02-25 16:44:16 +0300

    Added responce dto

---
+++ bb-backend/BLL.Models/EquiringResponce/EquiringResponceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BLL.Models.EquiringResponce;
+
+public class EquiringResponceDto
+{
+    public long ClickTransId { get; set; }
+    public string MerchantTransId {get;set;}		
+    public int MerchantConfirmId {get;set;}	
+    public int Error {get;set;}	
+    public string ErrorNote {get;set;}	
+}
