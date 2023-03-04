5f21645451b8673087b08b876962bca62e846142
ProdamGaraj <prodgaraj@yandex.ru>
2023-03-04 12:36:18 +0300

    Added new migration shell script updated

--- bb-backend/Infrastructure/Data/Migrations/20230304093226_PaymentEntities.Designer.cs
+++ bb-backend/Infrastructure/Data/Migrations/20230304093226_PaymentEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Infrastructure.Data.Migrations
 {
     [DbContext(typeof(BilimContext))]
-    partial class BilimContextModelSnapshot : ModelSnapshot
+    [Migration("20230304093226_PaymentEntities")]
+    partial class PaymentEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
